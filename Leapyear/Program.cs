1c4fffae4e55a2e4dab3f13c86bf959dea9282e4
Sripriya Sundararaman <spreiyah@yahoo.com>
2021-06-11 12:16:02 +1200

    Add project files.

---
+++ Leapyear/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Leapyear
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int year;
+            Console.WriteLine("Enter year");
+            year = Convert.ToInt32(Console.ReadLine());
+            if(year%4==0 && year%400==0 || year%100!=0)
+            {
+                Console.WriteLine("Entered year " + year + " is a leap year");
+            }
+            else
+            {
+                Console.WriteLine("Entered year " + year + " is not a leap year");
+            }
+        }
+    }
+}
